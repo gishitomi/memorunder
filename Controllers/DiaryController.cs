4f211b3fb3a2fcf3498db1fef5dc6f08649b114b
gisho1140 <shotagishitomi@gmail.com>
2022-03-29 00:41:20 +0900

    2022/3/29 新規作成

---
+++ Controllers/DiaryController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace memorunder.Controllers
+{
+  public class DiaryController : Controller
+  {
+    // GET: /<controller>/
+    //トップページ（日記一覧画面）
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
---
+++ Controllers/MemoController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace memorunder.Controllers
+{
+  public class MemoController : Controller
+  {
+    // GET: /<controller>/
+    //メモ一覧画面
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
